01ed0893a4b0c5628d39cad60e33b2b0d0f808a5
amir <amirhossein611@gmail.com>
2024-02-05 12:05:36 +0330

    side bar styles

--- src/App.js
+++ src/App.js
@@ -55,9 +55,9 @@ function App() {
 
   return (
     <Routes>
-      <Route path="/" element={<SearchPage />} />
+      <Route path="/" element={<ProfilePage />} />
       <Route path="/searchresultpage" element={<SearchResultPage />} />
-      <Route path="/profilepage" element={<ProfilePage />} />
+      <Route path="/profilepage" element={<SearchPage />} />
     </Routes>
   );
 }
